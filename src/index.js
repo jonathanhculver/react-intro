06365902e94345d9d944d5d425c003e85dea96e8
Jon Culver <jon@culverlabs.co>
2017-01-22 20:13:49 -0500

    s

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const App = function() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
   return <div>hi!</div>;
 }
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
